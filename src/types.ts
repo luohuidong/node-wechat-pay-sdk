d402b60e35bde00ba2c36710789378456fa244c2
luohuidong <luohuidong01@126.com>
2024-10-12 16:28:12 +0800

    feat: 新增微信支付通知请求体类型和resource解密后字段类型

--- src/types.ts
+++ src/types.ts
@@ -61,6 +61,66 @@ export interface PayTransactionsQueryByOutTradeNoResponseData {
   transaction_id: string;
 }
 
+/** 支付成功结果通知请求体 */
+export interface NotifyBody {
+  id: string;
+  create_time: string;
+  resource_type: string;
+  event_type: string;
+  summary: string;
+  resource: {
+    original_type: string;
+    algorithm: string;
+    ciphertext: string;
+    associated_data: string;
+    nonce: string;
+  };
+}
+
+/** 支付成功结果通知请求体 resource 解密后字段类型 */
+export interface NotifyResource {
+  transaction_id: string;
+  amount: {
+    payer_total: number;
+    total: number;
+    currency: string;
+    payer_currency: string;
+  };
+  mchid: string;
+  trade_state: "SUCCESS" | "REFUND" | "NOTPAY" | "CLOSED" | "REVOKED" | "USERPAYING" | "PAYERROR";
+  bank_type: string;
+  promotion_detail: {
+    amount: number;
+    wechatpay_contribute: number;
+    coupon_id: string;
+    scope: "GLOBAL" | "SINGLE";
+    merchant_contribute: number;
+    name: string;
+    other_contribute: number;
+    currency: string;
+    stock_id: string;
+    goods_detail: {
+      goods_remark: string;
+      quantity: number;
+      discount_amount: number;
+      goods_id: string;
+      unit_price: number;
+    }[];
+  }[];
+  success_time: string;
+  payer: {
+    openid: string;
+  };
+  out_trade_no: string;
+  AppID: string;
+  trade_state_desc: string;
+  trade_type: string;
+  attach: string;
+  scene_info: {
+    device_id: string;
+  };
+}
+
 export type Certificates = {
   serial_no: string;
   effective_time: string;
